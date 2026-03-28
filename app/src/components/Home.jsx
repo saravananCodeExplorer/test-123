741041f9f7bbe6ebf79c9b48eb6340619ce7d710
saravananCodeExplorer <153452081+saravananCodeExplorer@users.noreply.github.com>
2026-03-28 16:01:43 +0530

    Squashed commit of the following:
    commit dd247dfd056b07d8a1b011a49b4b2506bffb3eeb
    Author: saravananCodeExplorer <153452081+saravananCodeExplorer@users.noreply.github.com>
    Date:   Sat Mar 28 15:36:47 2026 +0530
    
        Services compo created
    
        few updated for Service

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import About from './About';
-import '../components/css/Home.css'; 
+import React from 'react'
+import About from './About'
 
 const Home = () => {
   const [count, setCount] = useState(0);
@@ -13,41 +12,11 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container">
-
-      {/* Navbar */}
-      <nav className="navbar">
-        <h2 className="logo">My App</h2>
-        <div className="nav-links">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-        </div>
-      </nav>
-
-      {/* Content */}
-      <h1 className="title">Welcome to the Home Page</h1>
-      <p className="text">This is a simple paragraph on the home page.</p>
-
-      {/* Counter */}
-      <h3>Counter: {count}</h3>
-      <button className="btn btn-inc" onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
-      <button className="btn btn-dec" onClick={() => setCount(count - 1)}>
-        Decrease
-      </button>
-
-      {/* Users */}
-      <h2>User List</h2>
-      <ul className="user-list">
-        {users.map(user => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      {/* About */}
-      <About name="Saravanan" role="Frontend Developer" />
-
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple paragraph on the home page.</p>
+          <About/>
+     
     </div>
   );
 };
