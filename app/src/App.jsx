27b4e4dc6558793ddfd8ee3ebeeca653cb91059a
saravananCodeExplorer <153452081+saravananCodeExplorer@users.noreply.github.com>
2026-03-28 14:58:33 +0530

    About created
    About created and update Home page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Home from './components/Home'
+
 
 const App = () => {
   return (
     <div>
       <h1>This is a simple React app</h1>
+      <Home/>
+  
     </div>
   )
 }
--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import About from './About'
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is a simple paragraph on the home page.</p>
+          <About/>
      
     </div>
   )
